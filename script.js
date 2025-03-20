1d26f53c0ca3e80203c14103d04c47b3e978882e
Saurabh Kumar <146050167+Saurabh-Kumar20-24@users.noreply.github.com>
2025-03-20 17:04:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //your JS code here. If requi
 function daysOfYear(year){
-	if((year%4==0 && year%100!=0)|| year%400==0){
-		return 365;
-	}else{
+	if((year%4===0 && year%100!=0)|| year%400==0){
 		return 366;
+	}else{
+		return 365;
 	}
 }
 
 let year=prompt("enter year")
 
-alert(daysOfYear());
+alert(daysOfYear(year));
